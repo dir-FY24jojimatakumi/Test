66d64cb84fa9af447bb3668acdbf2ff9b4638ae5
dir-FY24jojimatakumi <takumi.jojima.y033@dirbato.co.jp>
2024-04-08 11:58:36 +0900

    lessson finish

--- Test/src/calc/CalculatorTest.java
+++ Test/src/calc/CalculatorTest.java
@@ -1,19 +1,21 @@
 package calc;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
     @Test
     void testAdd() {
-        fail("まだ実装されていません");
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 3));
     }
 
     @Test
     void testSubtract() {
-        fail("まだ実装されていません");
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3));
     }
-    
 
 }
